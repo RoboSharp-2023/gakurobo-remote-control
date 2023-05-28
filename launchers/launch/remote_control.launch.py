bae7415681ca14feecb151e2485ec85f1c02df8f
naga <toritonsuibu0527@gmail.com>
2023-05-28 13:57:35 +0900

    node change

--- launchers/launch/remote_control.launch.py
+++ launchers/launch/remote_control.launch.py
@@ -9,34 +9,5 @@ def generate_launch_description():
                 executable='joy_node',
                 name='joy'
             ),
-            
-            Node(
-                package='float_to_uint8array',
-                executable='main',
-                exec_name='float_to_uint8array',
-                name='float_to_uint8array'
-            ),
-            
-            Node(
-                package='spd_to_omni',
-                executable='main',
-                exec_name='spd_to_omni',
-                name='spd_to_omni'
-            ),
-            
-            Node(
-                package='controller_node',
-                executable='main',
-                exec_name='controller_node',
-                name='controller_node',
-                output='screen'
-            ),
-            
-            Node(
-                package='integrate_message',
-                executable='main',
-                exec_name='integrate_msg',
-                name='integrate_msg',
-            ),
         ]
     )
--- launchers/launch/remote_control_robo.launch.py
+++ launchers/launch/remote_control_robo.launch.py
@@ -16,17 +16,46 @@ def generate_launch_description():
                 ]
             ),
             
+            # Node(
+            #     package='uart_communicator',
+            #     executable='main',
+            #     exec_name='uart_communicator2',
+            #     name='uart_communicator2',
+            #     output='screen',
+            #     emulate_tty=True,
+            #     parameters=[
+            #         {"serial_name": "/dev/ttyUSB0"},
+            #         {"uart_topic_name": "solenoid_uart_msg"},
+            #     ]
+            # ),
+            
             Node(
-                package='uart_communicator',
+                package='float_to_uint8array',
                 executable='main',
-                exec_name='uart_communicator2',
-                name='uart_communicator2',
-                output='screen',
-                emulate_tty=True,
-                parameters=[
-                    {"serial_name": "/dev/ttyUSB0"},
-                    {"uart_topic_name": "solenoid_uart_msg"},
-                ]
+                exec_name='float_to_uint8array',
+                name='float_to_uint8array'
+            ),
+            
+            Node(
+                package='spd_to_omni',
+                executable='main',
+                exec_name='spd_to_omni',
+                name='spd_to_omni'
+            ),
+            
+            Node(
+                package='controller_node',
+                executable='main',
+                exec_name='controller_node',
+                name='controller_node',
+                output='screen'
+            ),
+            
+            Node(
+                package='integrate_message',
+                executable='main',
+                exec_name='integrate_msg',
+                name='integrate_msg',
             ),
             
         ]
